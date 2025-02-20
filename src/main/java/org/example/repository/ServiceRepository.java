e44fd0147d433f15adc5e67e1339e7d59fba934c
DenisShakhurov <disa.sdf86@gmail.com>
2025-02-20 15:23:31 +0700

    #REF all repository

--- src/main/java/org/example/repository/ServiceRepository.java
+++ src/main/java/org/example/repository/ServiceRepository.java
@@ -2,6 +2,8 @@ package org.example.repository;
 
 import org.example.model.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ServiceRepository extends JpaRepository<Service, Long> {
 }
