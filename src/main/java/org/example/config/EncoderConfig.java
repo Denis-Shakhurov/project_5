9f7901a881be4df6afc64d6b7ae460f9d0fa38fe
DenisShakhurov <disa.sdf86@gmail.com>
2025-02-21 20:58:01 +0700

    #ADD BCryptPasswordEncoder

---
+++ src/main/java/org/example/config/EncoderConfig.java
@@ -0,0 +1,17 @@
+package org.example.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.*;
+
+@Configuration
+@RequiredArgsConstructor
+public class EncoderConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/org/example/mapper/UserMapper.java
+++ src/main/java/org/example/mapper/UserMapper.java
@@ -1,10 +1,12 @@
 package org.example.mapper;
 
+import lombok.RequiredArgsConstructor;
 import org.example.dto.user.UserCreateDTO;
 import org.example.dto.user.UserDTO;
 import org.example.dto.user.UserUpdateDTO;
 import org.example.model.User;
 import org.mapstruct.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Mapper(
         uses = { JsonNullableMapper.class, ReferenceMapper.class},
@@ -12,7 +14,10 @@ import org.mapstruct.*;
         componentModel = MappingConstants.ComponentModel.SPRING,
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
+@RequiredArgsConstructor
 public abstract class UserMapper {
+    private final BCryptPasswordEncoder passwordEncoder;
+
     public abstract UserDTO map(User model);
 
     public abstract User map(UserDTO dto);
@@ -20,4 +25,18 @@ public abstract class UserMapper {
     public abstract User map(UserCreateDTO dto);
 
     public abstract void update(UserUpdateDTO dto, @MappingTarget User model);
+
+    @BeforeMapping
+    private void encryptPassword(UserCreateDTO dto) {
+        String password = dto.getPassword();
+        dto.setPassword(passwordEncoder.encode(password));
+    }
+
+    @BeforeMapping
+    private void encryptPassword(UserUpdateDTO dto, @MappingTarget User model) {
+        if (dto.getPassword() != null && dto.getPassword().isPresent()) {
+            String password = dto.getPassword().get();
+            model.setPassword(passwordEncoder.encode(password));
+        }
+    }
 }
